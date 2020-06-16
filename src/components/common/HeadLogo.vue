a122f8fe90da88f899ea5834ab436882a78f4e35
zhaoyue <zhaoyue@itcast.cn>
2020-06-16 15:33:12 +0800

    头部组件 用户名字展示和退出

---
+++ src/components/common/HeadLogo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="head-box">
+        <span>欢迎您 , {{ userName }}</span>
+        <span class="sign-out" @click="logOut">退出</span>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name: 'HeadLogo',
+    computed: {
+        ...mapGetters(['userName']),
+    },
+    methods: {
+        logOut() {
+            this.$store.dispatch('user/logOut')
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../theme/base.less';
+.head-box {
+    height: 60px;
+    background-color: @headBarColor;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    .sign-out {
+        cursor: pointer;
+        color: #007cf9;
+        margin-left: 20px;
+    }
+}
+</style>
